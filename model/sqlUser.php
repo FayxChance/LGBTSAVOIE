42d85118bf35f9d1b265bcca7ed0fbe3089bdc58
Rémi <remibouvier99@gmail.com>
2018-11-21 10:23:03 +0100

    création fichier action inscription + ajout formulaire inscription
    création d'une requête d'insertion d'utilisateur dans la base de donnée.

--- model/sqlUser.php
+++ model/sqlUser.php
@@ -1,17 +1,22 @@
 <?php
-  echo getcwd();
   include "../includes/db.php";
   function selectUtilisateur($id)
   {
     global $c;
-    $select="SELECT (`nomUtilisateur`,`prenomUtilisateurs`,`pseudoUtilisateur`,`telUtilisateur`,`mailUtilisateur`,`mdpUtilisateur`,`roleUtilisateur`,`fonctionUtilisateur`) FROM Utilisateur (`nomUtilisateur`,`prenomUtilisateurs`,`pseudoUtilisateur`,`telUtilisateur`,`mailUtilisateur`,`mdpUtilisateur`,`roleUtilisateur`,`fonctionUtilisateur`)
-    WHERE id=$id";
+    $select="SELECT (`nomUtilisateur`,`prenomUtilisateurs`,`pseudoUtilisateur`,`telUtilisateur`,`mailUtilisateur`,`mdpUtilisateur`,`roleUtilisateur`) FROM Utilisateur (`nomUtilisateur`,`prenomUtilisateurs`,`pseudoUtilisateur`,`telUtilisateur`,`mailUtilisateur`,`mdpUtilisateur`,`roleUtilisateur`) WHERE id=$id";
     return mysqli_query($c, $select);
   }
 
 function selectAllUtilisateurs()
 {
   global $c;
-  $select="SELECT * FROM Utilisateur WHERE 1";
+  $select="SELECT (`nomUtilisateur`,`prenomUtilisateurs`,`pseudoUtilisateur`,`telUtilisateur`,`mailUtilisateur`,`mdpUtilisateur`,`roleUtilisateur`) FROM Utilisateur (`nomUtilisateur`,`prenomUtilisateurs`,`pseudoUtilisateur`,`telUtilisateur`,`mailUtilisateur`,`mdpUtilisateur`,`roleUtilisateur`)";
   return mysqli_query($c, $select) ;
-}
+ }
+  
+function insertUtilisateur($nomUtilisateur, $prenomUtilisateurs, $pseudoUtilisateur, $telUtilisateur, $mailUtilisateur, $mdpUtilisateur, $roleUtilisateur)
+ {
+	$inser= "INSERT INTO `Utilisateur`(`nomUtilisateur`,`prenomUtilisateurs`,`pseudoUtilisateur`,`telUtilisateur`, `mailUtilisateur`,`mdpUtilisateur`,`roleUtilisateur`)
+    VALUES(`$nomUtilisateur`,`$prenomUtilisateurs`,`$pseudoUtilisateur`,`$telUtilisateur`, `$mailUtilisateur`,`$mdpUtilisateur`,`$roleUtilisateur`)";
+    return mysqli_query($c, $inser) or die("Erreur envoie");
+ }
