2cf94d04761138e3e0d8c67feb80b72b59a34f86
Rémi <remibouvier99@gmail.com>
2018-11-21 08:32:10 +0100

    connexion form

--- includes/login.php
+++ includes/login.php
@@ -1,9 +1,14 @@
-<?php
-  session_start();
-  include "./includes/header.php";
-?>
-
-
-<?php
-  include "./includes/footer.php";
-?>
+<form method='post' action='./includes/login.php'>
+	<p>
+		<label for='pseudoLogin'>Pseudo</label>
+		<input type='text' name='pseudoLogin'/>
+	</p>
+	<p>
+		<label for='mdpLogin'>Mot de passe</label>
+		<input type='password' name='mdpLogin' size='16'/>
+	</p>
+	<p>
+		<label for='action'></label>
+		<input type='submit' name='action' value='Connexion'/>
+	</p>
+</form>
