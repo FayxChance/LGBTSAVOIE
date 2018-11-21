f3e258d2cf85fe2542e27f0dc05a97de3e90ac9e
Cthulhu <fayxchancetom@outlook.com>
2018-11-21 09:35:02 +0100

    connexion:ok, barre admin ok;

--- action/login.php
+++ action/login.php
@@ -1,13 +1,13 @@
 <?php
-session_start();
-include "./model/sqlUser.php";
-include "verif.php";
-
-
-while ($row = mysqli_fetch_assoc(selectAllUtilisateurs())) {
-		if $_POST['pseudoLogin']==$row['pseudoUtilisateur'] and $_POST['mdpLogin']==$row['mdpUtilisateur'] {
-		$_SESSION['connecte']=true;
-		$_SESSION['role']=$row['roleUtilisateur'];
+	session_start();
+	include "../model/sqlUser.php";
+	$res=selectAllUtilisateurs();
+	while ($row = mysqli_fetch_assoc($res)){
+		if( $_POST['pseudoLogin']==$row['pseudoUtilisateur'] && $_POST['mdpLogin']==$row['mdpUtilisateur'] ){
+			$_SESSION['connecte']=true;
+			$_SESSION['role']=$row['roleUtilisateur'];
 		}
-	}	
+		var_dump($_SESSION);
+	}
+	header("Location:../index.php");
 ?>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   include "./includes/header.php";
 ?>
 <?php
-  if ($_SESSION['role']=='admin') {
+  if ($_SESSION['role']==1) {
       include "./action/actionAdmin.php";
   } else {
       include "./action/actionUtilisateur.php";
