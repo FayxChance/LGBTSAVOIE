3437c50ae3bf650c1ed265fb241ffed2e5dd430c
Hamze <hamzaalrashed1@hotmail.com>
2018-11-26 15:34:08 +0100

    l'inscription

--- action/inscription.php
+++ action/inscription.php
@@ -1,3 +1,6 @@
 <?php
 
+    include_once "../model/sqlUser.php";
+    inserUtilisateur($_POST["nomUtilisateur"],$_POST["prenomUtilisateur"],$_POST["pseudoUtilisateur"],$_POST["mailUtilisateur"],$_POST["mdpUtilisateur"],$_POST["telUtilisateur"]);
+    header("Location:../../index.php?actionUtilisateur=index");
 ?>
