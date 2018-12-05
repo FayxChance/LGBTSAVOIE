373e63c7d17f96ceb7bbe970d4ea0ab319a67952
Rémi <remibouvier99@gmail.com>
2018-12-05 09:13:32 +0100

    galerie 18

--- action/Galerie/insertGalerie.php
+++ action/Galerie/insertGalerie.php
@@ -1,9 +1,9 @@
 <?php
-$PATH=realpath(NULL);
-echo($PATH);
+$PATH=realpath("/var/www/private/web/LGBTSAVOIE/LGBTSAVOIE/image");
 $nomIm=$_FILES['file']['name'];
 $cheminImage =$PATH."/".$nomIm;
 echo ($cheminImage);
+echo("</br>");
+echo($_FILES['file']['tmp_name']);
 move_upload_file($_FILES['file']['tmp_name'],$cheminImage);
-
 ?>
