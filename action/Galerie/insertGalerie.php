e975e0856f14d0b25342308ef34c164b0b5cf795
Rémi <remibouvier99@gmail.com>
2018-12-05 09:03:43 +0100

    galerie 15

--- action/Galerie/insertGalerie.php
+++ action/Galerie/insertGalerie.php
@@ -1,9 +1,8 @@
 <?php
-$PATH=realpath("./image");
+$PATH=realpath("./../../image");
 echo($PATH);
 $nomIm=$_FILES['file']['name'];
 $cheminImage =$PATH."/".$nomIm;
-echo($cheminImage);
 move_upload_file($_FILES['file']['tmp_name'],$cheminImage);
 
 ?>
