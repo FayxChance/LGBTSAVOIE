380bd4dddae31681ef2891fde9a7c1fc8be94225
Cthulhu <fayxchancetom@outlook.com>
2018-12-05 09:24:45 +0100

    modif css formulaire galerie

--- action/Galerie/actionGalerie.php
+++ action/Galerie/actionGalerie.php
@@ -1,6 +1,10 @@
+
+
+<div class="formulairesCIS">
 <form method="POST" action="./action/Galerie/insertGalerie.php" enctype="multipart/form-data">
 	<p> Ajouter une image </p>
 	<label for="file"/>Image</label>
 		<input  type="file" name="file"><br/>
 		<input  type="submit" name="insererGalerie" value="Upload une image"><br/>
 </form>
+</div>
