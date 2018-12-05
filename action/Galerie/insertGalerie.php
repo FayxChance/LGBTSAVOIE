50b9fef6cf62b67292a775baead8500e729f99fb
Cthulhu <fayxchancetom@outlook.com>
2018-12-05 09:48:06 +0100

    modif css

--- action/Galerie/insertGalerie.php
+++ action/Galerie/insertGalerie.php
@@ -1,9 +1,10 @@
 <?php
-$PATH=realpath('/../../images');
+$PATH=realpath('../../images');
 $nomIm=$_FILES['file']['name'];
 $cheminImage =$PATH."/".$nomIm;
 echo ($cheminImage);
 echo("</br>");
+var_dump($_FILES);
 echo($_FILES['file']['tmp_name']);
 move_uploaded_file($_FILES['file']['tmp_name'],$cheminImage);
 echo("yolo");
